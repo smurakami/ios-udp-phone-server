59c54590b13bab75ee07c0db9dad1ff6cd68d98e
s.murakami <smurakami.84@gmail.com>
2016-02-04 17:25:59 +0900

    発話の受信を実装

--- UDPServer/ViewController.swift
+++ UDPServer/ViewController.swift
@@ -7,10 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController, UDPDelegate {
     
     let udp = UDP()
+    
+    let engine = AVAudioEngine()
+    let player = AVAudioPlayerNode()
+    var outputBuffer = AVAudioPCMBuffer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +23,20 @@ class ViewController: UIViewController, UDPDelegate {
         udp.delegate = self
         print(udp.getIPAddress())
         udp.startServerOnPort(5000)
+        
+        
+        let bufferSize = UInt32(16537) // 決め打ち。ここを動的に変更できるようにはしたい。
+        outputBuffer = AVAudioPCMBuffer(PCMFormat: player.outputFormatForBus(0), frameCapacity: bufferSize)
+        outputBuffer.frameLength = bufferSize
+
+        
+        engine.attachNode(player)
+        engine.connect(player, to: engine.mainMixerNode, format: player.outputFormatForBus(0))
+        
+        engine.prepare()
+        try! engine.start()
+        
+        player.play()
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,11 +44,53 @@ class ViewController: UIViewController, UDPDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    
     // UDPDelegate
+    
+    var bufferOffset = 0
     
     func udp(udp: UDP!, didReceiveData data: NSData!, fromAddress addr: NSData!) {
         print("receive")
-        print(data)
+        
+        let count = Int(data.length / sizeof(Float))
+        
+        var received = [Float](count: count, repeatedValue: 0)
+        data.getBytes(&received, length: data.length)
+        
+        let bus = 0
+        print("data length: \(count)")
+        
+        let step = Int(self.engine.mainMixerNode.outputFormatForBus(bus).channelCount)
+        
+        var i = 0
+        
+        while i < count && i + bufferOffset < Int(self.outputBuffer.frameLength) {
+            let index = i + bufferOffset
+            self.outputBuffer.floatChannelData.memory[index] = received[i]
+            i += step
+        }
+        
+        print("buffer offset: \(bufferOffset)")
+        
+        bufferOffset = bufferOffset + i
+        
+        if bufferOffset >= Int(self.outputBuffer.frameLength) {
+            print("schedule")
+            self.player.scheduleBuffer(self.outputBuffer, atTime: nil, options: .InterruptsAtLoop, completionHandler: nil)
+            
+            var index = 0
+            while i < count {
+                self.outputBuffer.floatChannelData.memory[index] = received[i]
+                i += step
+                index += step
+            }
+            bufferOffset = index
+        }
+        
+//        for var i = 0; i < received.count; i += step {
+//            self.outputBuffer.floatChannelData.memory[i] = received[i]
+//        }
+//        self.player.scheduleBuffer(self.outputBuffer, atTime: nil, options: .Interrupts, completionHandler: nil)
     }
     
     func udp(udp: UDP!, didReceiveError error: NSError!) {
